1e8c60d0b3689b7d1634f47972904c67056db520
omarelwaliely <77898546+omarelwaliely@users.noreply.github.com>
2022-12-09 22:36:44 +0200

    added data structure for data and created access screen

--- cache.py
+++ cache.py
@@ -1,10 +1,19 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
+from collections import defaultdict
+import math
+import binascii
+
 filename = ""
 totalSize = 0
 lineSize = 0
 cycles = 0
+
+class Data:
+    validbit = '0'
+    tag = "N/A"
+    memory = None
 
 
 def fileclick():
@@ -12,12 +21,11 @@ def fileclick():
     filename = filedialog.askopenfilename()
 
 
-def simulation(line,size,cycle,frame,button):
+def simulation(line,size,cycle,frame,button,root):
     global filename
     global totalSize
     global lineSize
     global cycles
-
     if filename == "":
         messagebox.showerror(message="You did not select a file!",title = "File Error")
     elif line.get() == "" or cycle.get() == "" or size.get() == "":
@@ -32,13 +40,64 @@ def simulation(line,size,cycle,frame,button):
         lineSize = int(line.get())
         frame.destroy()
         button.destroy()
-        simulatorScreen()
+        simulatorScreen(root)
 
 
-def simulatorScreen():
+def simulatorScreen(root):
+    root.geometry("800x800")
+    try:
+        index = int(math.log2(totalSize/lineSize))
+        memory = []
+        for i in range((2**(index)-1)):
+           x = Data()
+           memory.append(x)
+        disp = math.log2(lineSize)
+        tag = int(32 - (index + disp))
+    except:
+        messagebox.showerror(message="Your sizes were insufficent please try again", title = "Incorrect Syntax")
+        root.destroy()
+        startScreen()
+        return
+
+
+    scrollframe = Scrollbar(root)
+    scrollframe.pack(side=RIGHT, fill=Y)
+    test= Text(root, width = 80, height = 80, wrap = NONE,
+                 yscrollcommand = scrollframe.set)
     with open(filename) as file:
-        for line in file:
-            print(line,end = '')
+        lines = (line.rstrip() for line in file) 
+        lines = (line for line in lines if line) #removes empty lines
+        j= 0 #number of accesses
+        test.insert(END,"Initial Data")
+        for i in range((2**index) -1):
+            test.insert(END,"\nIndex: "+ "{0:08b}".format(int(str(i),10)) + "   Valid bit: " + memory[i].validbit + "  Tag: "+ memory[i].tag + "\n")
+        for line in lines:
+            try:
+                curr = "{0:08b}".format(int(line, 16))
+                curr = curr.rjust(32, '0')
+                if(memory[int(curr[tag:(tag+index)],2)].tag == "N/A"):
+                    memory[int(curr[tag:(tag+index)],2) -1].tag = curr[:tag]
+                    memory[int(curr[tag:(tag+index)],2) -1].validbit = '1'
+                    print("BBING BONG TIME")
+                    missTest = "MISS"
+                elif(memory[int(curr[tag:(tag+index)],2) -1].tag== curr[:tag]):
+                    missTest = "HIT"
+                else:
+                    memory[int(curr[tag:(tag+index)],2) -1].tag = curr[:tag]
+                    memory[int(curr[tag:(tag+index)],2) -1].validbit = '1'
+                    missTest = "MISS"
+                test.insert(END,"Access #" + str(j+1) + "\n" + missTest)
+                j = j+1
+                for i in range((2**index) -1):
+                    test.insert(END,"\nIndex: "+ "{0:08b}".format(int(str(i),10)) + "   Valid bit: " + memory[i].validbit + "  Tag: "+ memory[i].tag + "\n")
+                test.insert(END,"\nHit Ratio: " +"Miss Ratio: "+ "AMAT: " + "\n\n")
+            except:
+                messagebox.showerror(message="You inputted the memory wrong!", title = "Incorrect Syntax")
+                return
+    test.pack()
+                
+
+
 
 
 
@@ -65,7 +124,7 @@ def startScreen():
     fileButton = Button(frame,text = "Select file",command=lambda: fileclick())
     fileLabel.grid(column=0,row=3)
     fileButton.grid(column=1,row=3)
-    startButton = Button(root, anchor="se",text = "Begin Simulation",command= lambda: simulation(lineEntry,sizeEntry,cycleEntry,frame,startButton))
+    startButton = Button(root, anchor="se",text = "Begin Simulation",command= lambda: simulation(lineEntry,sizeEntry,cycleEntry,frame,startButton,root))
     startButton.pack()
     root.mainloop()
 
